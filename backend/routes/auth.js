33095344e5a13d2f9d7dcedcca52d18ef03755b2
Maxim Tsirlin <maximtsirlin@gmail.com>
2023-08-05 21:35:52 +0300

    crash-test

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 const { login, createUser } = require('../controllers/login');
 const { validateLogin, validateRegister } = require('../utils/validators/userValidator');
 
+router.post('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 router.post('/signup', validateRegister, createUser);
 router.post('/signin', validateLogin, login);
 
